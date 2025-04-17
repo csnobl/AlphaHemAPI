5a057c3fbef1095101de928d5c7545fcfb171d3f
Christoffer Snobl <csnobl@hotmail.com>
2025-04-17 09:56:52 +0200

    Models, dbcontext

---
+++ AlphaHemAPI/Data/AlphaHemAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AlphaHemAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlphaHemAPI.Data
+{
+    public class AlphaHemAPIDbContext : DbContext
+    {
+        public AlphaHemAPIDbContext() : base()
+        {   
+        }
+
+        public DbSet<Listing> Listings { get; set; }
+        public DbSet<Realtor> Realtors { get; set; }
+        public DbSet<Agency> Agencies { get; set; }
+        public DbSet<Municipality> Municipalities { get; set; }
+    }
+
+    
+}
---
+++ AlphaHemAPI/Data/Models/Realtor.cs
@@ -0,0 +1,18 @@
+﻿namespace AlphaHemAPI.Data.Models
+{
+    public class Realtor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ProfilePicture { get; set; }
+
+        // Navigation properties
+
+        public int AgencyId { get; set; }
+        public Agency Agency { get; set; }
+        public List<Listing> Listings { get; set; }
+    }
+}

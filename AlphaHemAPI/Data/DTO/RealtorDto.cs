340f2ad7b5dbad988e4b0e6d106a4a136edb7bf0
Dominika Simson <dominika.simson1@yaelev.se>
2025-04-22 08:49:23 +0200

    list all realtors

--- AlphaHemAPI/Data/DTO/RealtorDto.cs
+++ AlphaHemAPI/Data/DTO/RealtorDto.cs
@@ -3,10 +3,12 @@
     public class RealtorDto
     {
         //Author: Mattias
+        //Co-author: Dominika
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string ProfilePicture { get; set; }
+        public string Agency { get; set; }
     }
 }
--- AlphaHemAPI/Data/Repositories/IRealtorRepository.cs
+++ AlphaHemAPI/Data/Repositories/IRealtorRepository.cs
@@ -3,8 +3,10 @@
 namespace AlphaHemAPI.Data.Repositories
 {
     // Author : Niklas
+    // Co-Author : Dominika
     public interface IRealtorRepository :IRepository<Realtor>
     {
         Task<Realtor?> GetByEmailAsync(string email);
+        Task<List<Realtor>> GetAllWithAgencyAsync();
     }
 }
